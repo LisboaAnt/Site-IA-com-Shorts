6a8bdeac9314cbb176a02dae00fffaa346028a54
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2023-09-16 20:01:13 -0300

    Final update

--- nlwia/executavel.bat
+++ nlwia/executavel.bat
@@ -1,7 +1,17 @@
 @echo off
+if not exist "node_modules" (
+	start /min "" cmd /c "npm install"
+	timeout /t 10 /nobreak >nul
+)
 start /min "" cmd /c "npm run web"
 start /min "" cmd /c "npm run server"
-timeout /t 1 /nobreak >nul
+
+
+REM Abrir a página web
+timeout /t 2 /nobreak >nul
 start "" http://localhost:5173/
+
+REM Esconde os terminais
+nircmd.exe win hide ititle "npm install"
 nircmd.exe win hide ititle "npm run web"
 nircmd.exe win hide ititle "npm run server"
